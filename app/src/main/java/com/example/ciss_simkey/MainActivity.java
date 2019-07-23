723bba01a2ba86d10252296803567f7d69457761
leo-1997 <36431769+leo-1997@users.noreply.github.com>
2019-07-24 01:19:41 +0800

    change format of first page

--- app/src/main/java/com/example/ciss_simkey/MainActivity.java
+++ app/src/main/java/com/example/ciss_simkey/MainActivity.java
@@ -12,6 +12,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Toast;
 
 import com.example.ciss_simkey.databinding.ActivityMainBinding;
@@ -30,13 +31,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+//        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,R.layout.main_title);
+        setTitle("选择程序");
         mBinding.selectionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
                 mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-//                List<PackageInfo> appsList = MainActivity.this.getPackageManager().getInstalledPackages(0);
                 List<ResolveInfo> appsList = MainActivity.this.getPackageManager().queryIntentActivities(mainIntent, 0);
                 showAppList(appsList);
             }
@@ -44,15 +48,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showAppList (final List<ResolveInfo> appList) {
-//        System.out.println("List is here " + appList.get(0).resolvePackageName);
-
-
         ArrayList<String> app = new ArrayList<>();
         int i = 0;
-        System.out.println("HERE");
         for (ResolveInfo info: appList) {
-//            ApplicationInfo appInfo = appList.get(i).activityInfo.applicationInfo;
-            //exclude system pre-installed apps
             if ((info.activityInfo.applicationInfo.flags & info.activityInfo.applicationInfo.FLAG_SYSTEM) <= 0 ) {
                 app.add(info.loadLabel(MainActivity.this.getPackageManager()).toString());
             }
@@ -63,16 +61,12 @@ public class MainActivity extends AppCompatActivity {
             i++;
         }
 
-        System.out.println("I am HERE!!!");
-//        System.out.println("here is the list " + apps);
-//        final String [] items = apps;
         AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
         alertBuilder.setTitle("AppList");
         alertBuilder.setItems(apps, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 startActivity(MainActivity.newIntent(MainActivity.this, apps[i]));
-//                Toast.makeText(MainActivity.this, apps[i], Toast.LENGTH_SHORT).show();
                 mAlert.dismiss();
             }
         });
